dad1efe714896a3230b7ee5337462bb4b66481fc
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-03-24 17:34:15 +0100

    add logistics class

--- LogisticCompany/Logistics.cs
+++ LogisticCompany/Logistics.cs
@@ -8,9 +8,30 @@ namespace LogisticCompany
 {
     public class Logistics
     {
+        //this method returns a type of product based on the transport type (Air, Sea, Road) based on the product class ProductTransport.cs
         public ProductTransport ManageTransport(string transportType)
         {
-            
+            ProductTransport transport = null;
+
+            //you can also writen an if-condition
+            switch (transportType)
+            {
+                case "Air":
+                    transport = new ConcreteFlightTransport();
+                    break;
+                case "Sea":
+                    transport = new ConcreteSeaTransport();
+                    break;                
+                case "Road":
+                    transport = new ConcreteRoadTransport();
+                    break;
+                default:
+                    throw new ArgumentException("Transport type not supported");
+            }
+
+            transport.CollectProductFromCustomer();
+            transport.TransportProducts();
+            return transport;
         }
     }
 }
